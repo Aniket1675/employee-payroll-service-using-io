401454624190092ad780cb96b2f900b7e89adc8f
Aniket1675 <Aniket@05>
2022-04-14 15:18:16 +0530

    welcome to employee payroll services using IO

---
+++ src/main/java/com/bridgelabz/EmployeePayRollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeePayRollService {
+    public static void main(String[] args) {
+        System.out.println("welcome to employee payroll services using IO");
+    }
+}
